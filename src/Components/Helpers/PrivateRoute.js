8e30510dbff4a378032611214f7a00de0e9a5d7e
venkatmamid <venkat.m@tekyantra.com>
2021-06-14 20:49:36 +0530

    Moving authcontext to private/public routes

--- src/Components/Helpers/PrivateRoute.js
+++ src/Components/Helpers/PrivateRoute.js
@@ -1,30 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
+import AuthContext from "../../store/auth-context";
 import { Route, Redirect } from "react-router-dom";
 
-const PrivateRoute = ({ component: Component, roles, authCtx, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) => {
-      const currentUser = authCtx.currentUser;
-      if (!currentUser) {
-        // not logged in so redirect to login page with the return url
-        return (
-          <Redirect
-            to={{ pathname: "/login", state: { from: props.location } }}
-          />
-        );
-      }
+const PrivateRoute = ({ component: Component, roles, name, ...rest }) => {
+  const authCtx = useContext(AuthContext);
 
-      // check if route is restricted by role
-      if (roles && roles.indexOf(authCtx.role) === -1) {
-        // role not authorised so redirect to home page
-        return <Redirect to={{ pathname: "/" }} />;
-      }
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        const currentUser = authCtx.currentUser;
+        if (!currentUser) {
+          // not logged in so redirect to login page with the return url
+          return (
+            <Redirect
+              to={{ pathname: "/login", state: { from: props.location } }}
+            />
+          );
+        }
 
-      // authorised so return component
-      return <Component {...props} />;
-    }}
-  />
-);
+        // check if route is restricted by role
+        if (roles && roles.indexOf(authCtx.role) === -1) {
+          // role not authorised so redirect to home page
+          return <Redirect to={{ pathname: "/" }} />;
+        }
+
+        // authorised so return component
+        return <Component {...props} />;
+      }}
+    />
+  );
+};
 
 export default PrivateRoute;
